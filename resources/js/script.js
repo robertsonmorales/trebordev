6fdd49b5fc617aeba3f4fc66e1d75e38d45d2d35
RobertPujante <moralesso152842@gmail.com>
2020-11-22 02:11:52 +0800

    added new content

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,23 @@
+function navSelected(id){
+    let navbar = document.getElementsByClassName('navbar');
+    for (let i = 0; i < navbar.length; i++) {
+        navbar[i].classList.remove('nav-selected');
+    }
+
+    let el = document.getElementById(id);
+    el.classList.add('nav-selected');
+}
+
 $(document).ready(function(){
-    
+    $( function(){
+        var path = window.location.href;
+        var newPath = path.split('/').reverse();
+        var nav = newPath[0].replace('#', 'nav-');
+
+        if(!nav){
+            $('#nav-home').addClass('nav-selected');
+        }else{
+            $('#'+nav).addClass('nav-selected');
+        }
+    });
 });
