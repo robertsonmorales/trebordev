c9e034472ae2818b9e16637a715a97f11feb2545
RobertPujante <moralesso152842@gmail.com>
2020-11-22 02:15:14 +0800

    fixed js load

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,17 +7,3 @@ function navSelected(id){
     let el = document.getElementById(id);
     el.classList.add('nav-selected');
 }
-
-$(document).ready(function(){
-    $( function(){
-        var path = window.location.href;
-        var newPath = path.split('/').reverse();
-        var nav = newPath[0].replace('#', 'nav-');
-
-        if(!nav){
-            $('#nav-home').addClass('nav-selected');
-        }else{
-            $('#'+nav).addClass('nav-selected');
-        }
-    });
-});
