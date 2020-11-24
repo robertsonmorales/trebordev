3302cbe351bb29fe6e003e92c9285e782e01a803
RobertPujante <moralesso152842@gmail.com>
2020-11-24 15:04:58 +0800

    new ui design

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,3 +7,46 @@ function navSelected(id){
     let el = document.getElementById(id);
     el.classList.add('nav-selected');
 }
+
+$(document).ready(function(){
+
+    $('#name').on('keyup', function(){
+        validator('name', $(this).val());
+    });
+
+    $('#email').on('keyup', function(){
+        validator('email', $(this).val());
+    });
+
+    $('#address').on('keyup', function(){
+        validator('address', $(this).val());
+    });
+
+    $('#message').on('keyup', function(){
+        validator('message', $(this).val());
+    });
+
+    function validator(inputName, inputValue){
+        let value = inputValue.trim();
+        
+        if(value == null || value == undefined || value == ""){
+            $('#error-'+inputName).html(inputName.charAt(0).toUpperCase() + inputName.substr(1) + ' field is required.');
+        }else{
+            $('#error-'+inputName).empty();
+        }
+    }
+
+    $('#btn-submit').on('click', function(event){
+        event.preventDefault()
+        var name = validator('name', $('#name').val());
+        var email = validator('email', $('#email').val());
+        var address = validator('address', $('#address').val());
+        var message = validator('message', $('#message').val());
+
+        if(name && email && address && message){
+            $(this).prop('disabled', true);
+            $('#submit-message').html('Submitting Message..');
+            $('#spinner').removeClass('d-none');
+        }
+    });
+});
